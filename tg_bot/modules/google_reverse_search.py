84085e01daccb64dc74fe99fb4260d87e3640db1
PANDITHAN <91892960+PANDITHAN@users.noreply.github.com>
2021-11-20 17:40:12 +0530

    Found

--- tg_bot/modules/google_reverse_search.py
+++ tg_bot/modules/google_reverse_search.py
@@ -185,7 +185,7 @@ __help__ = """
 - /reverse: Does a reverse image search of the media which it was replied to.
 """
 
-__mod_name__ = "IMAGE SEARCH"
+__mod_name__ = "𝙸𝙼𝙰𝙶𝙴 𝚂𝙴𝙰𝚁𝙲𝙷"
 
 REVERSE_HANDLER = DisableAbleCommandHandler("reverse", reverse, pass_args=True, admin_ok=True)
 
