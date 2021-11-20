3adc2a816a3f9f0d9c4e4cb8f068fe0f8f17c0d8
PANDITHAN <91892960+PANDITHAN@users.noreply.github.com>
2021-11-20 23:36:25 +0530

    Text style

--- tg_bot/modules/covid.py
+++ tg_bot/modules/covid.py
@@ -14,7 +14,7 @@ from urllib.request import urlopen
 @run_async
 def covid(bot: Bot, update: Update):
     message = update.effective_message
-    device = message.text[len('/covid '):]
+    device = message.text[len(' /deploy '):]
     fetch = get(f'https://coronavirus-tracker-api.herokuapp.com/all')
 
     if fetch.status_code == 200:
@@ -25,11 +25,7 @@ def covid(bot: Bot, update: Update):
         total_deaths_global = parsed["latest"]["deaths"]
         total_recovered_global = parsed["latest"]["recovered"]
         active_cases_covid19 = total_confirmed_global - total_deaths_global - total_recovered_global
-        reply_text = ("*Corona Stats🦠:*\n"
-        "Total Confirmed: `" + str(total_confirmed_global) + "`\n"
-        "Total Deaths: `" + str(total_deaths_global) + "`\n"
-        "Total Recovered: `" + str(total_recovered_global) +"`\n"
-        "Active Cases: `"+ str(active_cases_covid19) + "`")
+        reply_text = ("HOW TO DEPLOY TUTORIAL VEDIO =>")
         message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
 
         return
@@ -44,7 +40,7 @@ __help__ = """
  - /covid get worldwide corona status
 """
 
-__mod_name__ = 'COVID-19'
+__mod_name__ = '𝙳𝙴𝙿𝙻𝙾𝚈'
 
 COVID_HANDLER = CommandHandler("covid", covid, admin_ok=True)
 dispatcher.add_handler(COVID_HANDLER)
