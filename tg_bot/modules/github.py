25e2bb51304c0f86bb4d6bd9ec1399c430d74f5b
PANDITHAN <91892960+PANDITHAN@users.noreply.github.com>
2021-11-20 17:37:01 +0530

    Found

--- tg_bot/modules/github.py
+++ tg_bot/modules/github.py
@@ -39,7 +39,7 @@ __help__ = """
  - /git:{GitHub username} Returns info about a GitHub user or organization.
 """
 
-__mod_name__ = "GITHUB"
+__mod_name__ = "𝙶𝙸𝚃𝙷𝚄𝙱"
 
 github_handle = DisableAbleCommandHandler("git", github)
 
