ed2ff67f80fdf4a9f11d41025761f4ebca604bd0
PANDITHAN <91892960+PANDITHAN@users.noreply.github.com>
2021-11-20 17:00:31 +0530

    Found

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -97,7 +97,7 @@ dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
 dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
 
-__mod_name__ = "AFK"
+__mod_name__ = "𝙰𝙵𝙺"
 __command_list__ = ["afk"]
 __handlers__ = [(AFK_HANDLER, AFK_GROUP), (AFK_REGEX_HANDLER, AFK_GROUP), (NO_AFK_HANDLER, AFK_GROUP),
                 (AFK_REPLY_HANDLER, AFK_REPLY_GROUP)]
