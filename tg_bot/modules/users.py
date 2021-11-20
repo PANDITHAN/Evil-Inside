78fd03ce4c4a67b874b36895cc3ee91c3c4626c1
PANDITHAN <91892960+PANDITHAN@users.noreply.github.com>
2021-11-20 17:57:26 +0530

    Found

--- tg_bot/modules/users.py
+++ tg_bot/modules/users.py
@@ -121,5 +121,5 @@ dispatcher.add_handler(USER_HANDLER, USERS_GROUP)
 dispatcher.add_handler(BROADCAST_HANDLER)
 dispatcher.add_handler(CHATLIST_HANDLER)
 
-__mod_name__ = "USERS"
+__mod_name__ = "𝚄𝚂𝙴𝚁𝚂"
 __handlers__ = [(USER_HANDLER, USERS_GROUP), BROADCAST_HANDLER, CHATLIST_HANDLER]
