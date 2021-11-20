93c13398a56757110a93f73a78abc0f36f48afa0
PANDITHAN <91892960+PANDITHAN@users.noreply.github.com>
2021-11-20 17:47:31 +0530

    Found

--- tg_bot/modules/muting.py
+++ tg_bot/modules/muting.py
@@ -204,5 +204,5 @@ dispatcher.add_handler(MUTE_HANDLER)
 dispatcher.add_handler(UNMUTE_HANDLER)
 dispatcher.add_handler(TEMPMUTE_HANDLER)
 
-__mod_name__ = "MUTES"
+__mod_name__ = "𝙼𝚄𝚃𝙴𝚂"
 __handlers__ = [MUTE_HANDLER, UNMUTE_HANDLER, TEMPMUTE_HANDLER]
