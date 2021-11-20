0028d3ee908c9631a74b70f446a02ce0e630f739
PANDITHAN <91892960+PANDITHAN@users.noreply.github.com>
2021-11-20 21:04:18 +0530

    Text style

--- tg_bot/modules/backups.py
+++ tg_bot/modules/backups.py
@@ -78,5 +78,5 @@ EXPORT_HANDLER = CommandHandler("export", export_data)
 dispatcher.add_handler(IMPORT_HANDLER)
 dispatcher.add_handler(EXPORT_HANDLER)
 
-__mod_name__ = "BACKUP"
+__mod_name__ = "𝙱𝙰𝙲𝙺𝚄𝙿"
 __handlers__ = [IMPORT_HANDLER, EXPORT_HANDLER]
