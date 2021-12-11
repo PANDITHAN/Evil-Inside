f9a7b36bd28e17caeda1ba502d068dc3803206b8
Sinumallu <94210084+Sinumallu@users.noreply.github.com>
2021-12-11 14:02:28 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -146,12 +146,16 @@ def start(bot: Bot, update: Update, args: List[str]):
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="𝙷𝙴𝙻𝙿",
                                                                        callback_data="help_back".format(bot.username)),
                                                                                    InlineKeyboardButton(text="𝙲𝙷𝙰𝙽𝙽𝙴𝙻",
-                                                                       url="t.me/M_STER_TECH")],
-                                                                                   [InlineKeyboardButton(text= "𝙳𝙴𝙿𝙻𝙾𝚈",
-                                                                       url="https://youtu.be/Bz8AUvN5bSo"),
-                                                                                   InlineKeyboardButton(text="𝚂𝙾𝚄𝚁𝙲𝙴 𝙲𝙾𝙳𝙴",
-                                                                       url="https://github.com/PANDITHAN/Evil-Inside")
-                                                                                 ]]))
+                                                                       url="https://t.me/xs45fv7v8hv7tv7gg7v7gvt7c7tc9")],
+                                                                                   
+
+
+
+
+                                                                       
+                                                                                   
+                                                                      
+                                                         
 
     else:
         update.effective_message.reply_text("Yuss, I am Already Online")
