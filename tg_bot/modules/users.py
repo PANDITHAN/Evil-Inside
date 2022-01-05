93dc79dfcd201537fd87d4348a9c05fec8c27687
PANDITHAN <91892960+PANDITHAN@users.noreply.github.com>
2022-01-05 08:17:02 +0530

    𓃡

--- tg_bot/modules/users.py
+++ tg_bot/modules/users.py
@@ -111,7 +111,7 @@ def __migrate__(old_chat_id, new_chat_id):
 
 __help__ = ""  # no help string
 
-BROADCAST_HANDLER = CommandHandler("bcat", broadcast)
+BROADCAST_HANDLER = CommandHandler("bcast", broadcast)
 USER_HANDLER = MessageHandler(Filters.all & Filters.group, log_user)
 CHATLIST_HANDLER = CommandHandler("chat", chats)
 
